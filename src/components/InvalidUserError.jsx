f66ab6e0432932f213e7a5305708a45eb7386c96
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-11-10 17:04:23 +0100

    Add error if user is not a part of the board

---
+++ src/components/InvalidUserError.jsx
@@ -0,0 +1,17 @@
+import "./InvalidUserError.css";
+
+const InvalidUserError = ({ history }) => {
+  return (
+    <div className="invalid-user-container">
+      <p
+        className="invalid-user-home-link"
+        onClick={() => history.push("/home")}
+      >
+        It looks like you are not a part of this board! Click here to create
+        your own board.
+      </p>
+    </div>
+  );
+};
+
+export default InvalidUserError;
--- src/pages/BoardView.jsx
+++ src/pages/BoardView.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Board, Card } from "../models/index";
 import { DataStore } from "@aws-amplify/datastore";
+import { useSelector } from "react-redux";
+
+import "./BoardView.css";
 
 import Navigation from "../components/Navigation";
 import Loading from "../components/Loading";
+import InvalidUserError from "../components/InvalidUserError"
 
 const BoardView = ({ history, match }) => {
-  const [loading, setLoading] = useState(true);
+  const { user } = useSelector((state) => state.user);
+  const [userValid, setUserValid] = useState(true);
+  const [loadingBoard, setLoadingBoard] = useState(true);
+  const [loadingCards, setLoadingCards] = useState(true);
   const [board, setBoards] = useState([]);
   const [cards, setCards] = useState([]);
 
-  const loadBoards = async () => {
+  const loadBoard = async () => {
     const boardQuery = await DataStore.query(Board, (b) =>
       b.id("eq", match.params.id)
     );
     console.log("boardQuery: ", JSON.stringify(boardQuery));
     setBoards(boardQuery[0]);
+    setLoadingBoard(false);
   };
 
   const loadCards = async () => {
@@ -24,31 +32,51 @@ const BoardView = ({ history, match }) => {
     );
     console.log("cardsQuery: ", JSON.stringify(cardsQuery));
     setCards(cardsQuery);
-    setLoading(false);
+    setLoadingCards(false);
+  };
+
+  const createCard = async () => {
+    const newCard = await DataStore.save(
+      new Card({
+        boardID: match.params.id,
+        title: "A card added to first board in boards list",
+        status: "TODO",
+        startDate: "2020-11-10",
+        endDate: "2020-12-31",
+      })
+    );
+    loadCards();
   };
 
   useEffect(() => {
-    if (loading) {
-      loadBoards();
-      loadCards();
-    }
+    if (loadingBoard) loadBoard();
+    if (loadingCards) loadCards();
   });
 
+  useEffect(() => {
+    if (!loadingBoard) setUserValid(board.users.includes(user.name));
+  }, [board]);
+
   return (
-    <div className="board-view-page-container">
-      {loading ? (
-        <div className="board-view-page loading-div">
+    <React.Fragment>
+      {(loadingCards || loadingBoard) ? ( 
+        <div className="board-view-page"> {/* display spinner when loading */}
           <Navigation history={history} />
           <Loading />
         </div>
-      ) : (
-        <div className="board-view-page">
+      ) : ( userValid ?  
+        <div className="board-view-page"> {/* if all is loaded and user is a part of the board display it */}
           <Navigation history={history} />
-          <p>{JSON.stringify(board)}</p>
-          <p>{JSON.stringify(cards)}</p>
-        </div>
+          <button onClick={createCard}>Create a card for this board</button>
+          <p>Board: {JSON.stringify(board)}</p>
+          <p>Cards:{cards.map((card) => { return <li>{JSON.stringify(card)}</li>})}</p>
+        </div> :
+         <div className="board-view-page-invalid-user-error"> {/* if user is not a part of the board they cannot see it */}
+         <Navigation history={history} />
+         <InvalidUserError history={history} />
+       </div>
       )}
-    </div>
+     </React.Fragment>
   );
 };
 
