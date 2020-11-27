6b5a849328e16ad6bff08b0755a0dffec3236698
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-11-27 22:25:50 +0100

    Add delete card button with full functionality

--- src/components/UserCardDialog.jsx
+++ src/components/UserCardDialog.jsx
@@ -17,18 +17,20 @@ import {
   ClickAwayListener
 } from "@material-ui/core";
 // GraphQL
-import { Card } from "../models/index";
+import { Card, Board } from "../models/index";
 import { DataStore } from "@aws-amplify/datastore";
 // Redux 
 import store from '../store';
 // CSS
 import "./UserCardDialog.css";
 import { cardTitleEditTheme } from "../themes/cardTitleEditTheme";
+import {deleteButtonTheme} from "../themes/deleteButtonTheme";
 import DescriptionIcon from '@material-ui/icons/Description';
 import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import { useSelector } from "react-redux";
 
 const UserCardDialog = ({ showUserCardDialog, closeUserCardDialog, card }) => {
-
+  const { id } = useSelector((state) => state.board);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [startDate, setStartDate] = useState("");
@@ -37,33 +39,33 @@ const UserCardDialog = ({ showUserCardDialog, closeUserCardDialog, card }) => {
   const [tag, setTag] = useState("");
   const [users, setUsers] = useState([]);
 
-  const handleEnter = () =>{
+  const handleEnter = () => {
     console.log(card);
     setTitle(card.title);
     setDescription(card.description);
     setStartDate(card.startDate);
-    setEndDate(card.endDate);    
-    setStatus(card.status);    
+    setEndDate(card.endDate);
+    setStatus(card.status);
     setTag(card.tag);
     setUsers(card.users);
-  }
+  };
 
-  const handleClose = () =>{
+  const handleClose = () => {
     // setTitle("");
     // setDescription("");
     // setStartDate("");
-    // setEndDate("");    
-    // setStatus("");    
+    // setEndDate("");
+    // setStatus("");
     // setTag("");
     // setUsers([]);
     closeUserCardDialog();
-  }
+  };
 
-  const descriptionClickAway = async () =>{
+  const descriptionClickAway = async () => {
     console.log("clicked away from description");
     const newDescription = description.trim();
-    
-    if(newDescription !== card.description){
+
+    if (newDescription !== card.description) {
       console.log("UPDATING DESCRIPTION");
       const cardQuery = await DataStore.query(Card, (c) => c.id("eq", card.id));
 
@@ -72,16 +74,16 @@ const UserCardDialog = ({ showUserCardDialog, closeUserCardDialog, card }) => {
           updated.description = newDescription;
         })
       );
-    }
-    else{
+    } else {
       setDescription(description);
     }
-  }
-  const titleClickAway = async () =>{
+  };
+
+  const titleClickAway = async () => {
     console.log("clicked away from title");
     const newTitle = title.trim();
-    
-    if(newTitle !== card.title && newTitle !== null && newTitle !== ""){
+
+    if (newTitle !== card.title && newTitle !== null && newTitle !== "") {
       console.log("UPDATING TITLE");
       const cardQuery = await DataStore.query(Card, (c) => c.id("eq", card.id));
 
@@ -90,11 +92,26 @@ const UserCardDialog = ({ showUserCardDialog, closeUserCardDialog, card }) => {
           updated.title = newTitle;
         })
       );
-    }
-    else{
+    } else {
       setTitle(title);
     }
-  }
+  };
+
+  const deleteCard = async () => {
+    const cardQuery = await DataStore.query(Card, (c) => c.id("eq", card.id));
+    console.log(card.id);
+    const boardQuery = await DataStore.query(Board, (b) => b.id("eq", id));
+
+    await DataStore.save(
+      Board.copyOf(boardQuery[0], (updated) => {
+        const index = updated.cards.indexOf(card.id);
+        updated.cards.splice(index, 1);
+      })
+    );
+
+    DataStore.delete(cardQuery[0]);
+    handleClose();
+  };
 
   return (
     <Dialog
@@ -107,12 +124,12 @@ const UserCardDialog = ({ showUserCardDialog, closeUserCardDialog, card }) => {
       <MuiThemeProvider theme={cardTitleEditTheme}>
         <DialogTitle className="user-card-dialog-title-container">
           <ClickAwayListener onClickAway={titleClickAway}>
-            <TextField 
-              onChange={(e) => setTitle(e.target.value)} 
+            <TextField
+              onChange={(e) => setTitle(e.target.value)}
               value={title}
               variant="outlined"
-              className="user-card-dialog-title">
-            </TextField>
+              className="user-card-dialog-title"
+            ></TextField>
           </ClickAwayListener>
           <Typography component={"span"} className="user-card-dialog-status">
             {card.status}
@@ -124,23 +141,20 @@ const UserCardDialog = ({ showUserCardDialog, closeUserCardDialog, card }) => {
         <DialogContentText>
           <Grid justify="center" container spacing={1}>
             <Grid item>
-                <Typography component={"span"}>
-                  {card.startDate}
-                </Typography>
+              <Typography component={"span"}>{card.startDate}</Typography>
             </Grid>
-            <Grid item >
-                <Typography component={"span"}>
-                -
-              </Typography>
+            <Grid item>
+              <Typography component={"span"}>-</Typography>
             </Grid>
-            <Grid item >
-                <Typography component={"span"}>
-                {card.endDate}
-              </Typography>
+            <Grid item>
+              <Typography component={"span"}>{card.endDate}</Typography>
             </Grid>
           </Grid>
         </DialogContentText>
-        <Typography className="user-card-description-label" component={'span'}><DescriptionIcon />Description</Typography>
+        <Typography className="user-card-description-label" component={"span"}>
+          <DescriptionIcon />
+          Description
+        </Typography>
         <ClickAwayListener onClickAway={descriptionClickAway}>
           <TextField
             className="user-card-description-dialog-textfield"
@@ -154,45 +168,36 @@ const UserCardDialog = ({ showUserCardDialog, closeUserCardDialog, card }) => {
             value={description}
           />
         </ClickAwayListener>
-         <Typography className="user-card-points-label" component={'span'}><FormatListBulletedIcon />Points</Typography>
-         <FormGroup>
-         <FormControlLabel
-          control={
-            <Checkbox
-              name="checkedB"
-              color="primary"
-            />
-          }
-          label="Point 1"
-        />
-         <FormControlLabel
-          control={
-            <Checkbox
-              name="checkedB"
-              color="primary"
-            />
-          }
-          label="Point 2"
-        />
-         <FormControlLabel
-          control={
-            <Checkbox
-              name="checkedB"
-              color="primary"
-            />
-          }
-          label="Point 3"
-        />
-         </FormGroup>
+        <Typography className="user-card-points-label" component={"span"}>
+          <FormatListBulletedIcon />
+          Points
+        </Typography>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox name="checkedB" color="primary" />}
+            label="Point 1"
+          />
+          <FormControlLabel
+            control={<Checkbox name="checkedB" color="primary" />}
+            label="Point 2"
+          />
+          <FormControlLabel
+            control={<Checkbox name="checkedB" color="primary" />}
+            label="Point 3"
+          />
+        </FormGroup>
       </DialogContent>
       <DialogActions>
-        {/* <Button
-          onClick={handleClose}
-          color="primary"
-          variant="outlined"
-        >
-          Cancel
-        </Button> */}
+        <MuiThemeProvider theme={deleteButtonTheme}>
+          <Button
+            onClick={deleteCard}
+            color="primary"
+            variant="outlined"
+            className="delete-card-dialog-delete-button"
+          >
+            Delete card
+          </Button>
+        </MuiThemeProvider>
       </DialogActions>
     </Dialog>
   );
