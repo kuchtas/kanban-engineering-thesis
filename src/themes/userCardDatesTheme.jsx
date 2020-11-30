1b809b208945e66de978e57ceabe85c5a65e6a74
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-11-30 16:58:53 +0100

    Add editable dates

---
+++ src/themes/userCardDatesTheme.jsx
@@ -0,0 +1,39 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+export const userCardDatesTheme = createMuiTheme({
+  overrides: {
+    MuiInputLabel: {
+      // Name of the component ⚛️ / style sheet
+      root: {
+        // Name of the rule
+        "&$focused": {
+          // increase the specificity for the pseudo class
+          color: "var(--amplify-primary-color)",
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      root: {
+        "& $notchedOutline": {
+          borderColor: "white",
+          backgroundColor: "rgba(224, 224, 231, 0.3)",
+        },
+        "&:hover:not($disabled):not($focused):not($error) $notchedOutline": {
+          borderColor: "white",
+          backgroundColor: "rgba(224, 224, 231, 0.5)",
+          // Reset on touch devices, it doesn't add specificity
+        },
+        "&$focused $notchedOutline": {
+          borderColor: "var(--amplify-primary-color)",
+        },
+      },
+      inputMarginDense: {
+        paddingTop: "5px",
+      },
+    },
+    MuiDialogContentText: {
+      root: {
+        marginBottom: "0px",
+      },
+    },
+  },
+});
