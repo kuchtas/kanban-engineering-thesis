ea73e67b9927a83be3d62654a8273fde6dbc1902
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-12-03 20:37:19 +0100

    Implement logic of calculating the CSS classes depending on the deadline closeness

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -14,26 +14,46 @@ const UserCard = ({
   tag,
   users,
   points,
-  index
+  index,
 }) => {
-  let deadlineCloseness = Math.round(
-    ((new Date() - new Date(startDate).setHours(0, 0, 0, 0)) /
-      (new Date(endDate).setHours(23, 59, 59, 59) -
-        new Date(startDate).setHours(0, 0, 0, 0))) *
-      100
-  );
-  if (deadlineCloseness === Infinity) deadlineCloseness = 1;
-  console.log(title, deadlineCloseness);
   const setClassByDeadlineCloseness = () => {
-    if (deadlineCloseness > 0 && deadlineCloseness <= 25)
-      return "deadline-term-1";
-    else if (deadlineCloseness > 25 && deadlineCloseness <= 50)
-      return "deadline-term-2";
-    else if (deadlineCloseness > 50 && deadlineCloseness <= 75)
-      return "deadline-term-3";
-    else if (deadlineCloseness > 75 && deadlineCloseness <= 100)
-      return "deadline-term-4";
-    else if (deadlineCloseness > 100) return "deadline-missed";
+    if (new Date(endDate).setHours(23, 59, 59, 59) -
+        new Date(startDate).setHours(0, 0, 0, 0) > 604800000) { //seven days
+      let deadlineCloseness = Math.round(
+        ((new Date() - new Date(startDate).setHours(0, 0, 0, 0)) /
+          (new Date(endDate).setHours(23, 59, 59, 59) -
+            new Date(startDate).setHours(0, 0, 0, 0))) *  100);
+            console.log(title, deadlineCloseness);
+          if (deadlineCloseness > 0 && deadlineCloseness <= 25)
+            return "deadline-term-1";
+          else if (deadlineCloseness > 25 && deadlineCloseness <= 50)
+            return "deadline-term-2";
+          else if (deadlineCloseness > 50 && deadlineCloseness <= 75)
+            return "deadline-term-3";
+          else if (deadlineCloseness > 75 && deadlineCloseness <= 100)
+            return "deadline-term-4";
+          else if (deadlineCloseness > 100) 
+            return "deadline-missed";
+    }   
+    
+    else if (new Date(endDate).setHours(23, 59, 59, 59) - // less 7 days left - 1
+             new Date() < 0)
+              return "deadline-missed";
+    else if (new Date(startDate).setHours(0, 0, 0, 0) -
+              new Date() > 0)
+               return;
+    else if (new Date(endDate).setHours(23, 59, 59, 59) -
+             new Date(startDate).setHours(0, 0, 0, 0) > 518400000) // less 6 days left - 2
+              return "deadline-term-1";
+    else if (new Date(endDate).setHours(23, 59, 59, 59) -
+             new Date(startDate).setHours(0, 0, 0, 0) > 345600000) // less 4 days left - 3
+              return "deadline-term-2";
+    else if (new Date(endDate).setHours(23, 59, 59, 59) -
+              new Date(startDate).setHours(0, 0, 0, 0) > 172800000) // less 2 days left - 4
+              return "deadline-term-3";
+    else if (new Date(endDate).setHours(23, 59, 59, 59) -
+              new Date(startDate).setHours(0, 0, 0, 0) > 0)
+              return "deadline-term-4";
   };
   return (
     <Draggable draggableId={id} index={index}>
