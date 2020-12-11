c7191d624d9d126206756890c40aeccaf732b94c
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-12-11 19:05:58 +0100

    Improve charts - still have to decide whether to put labels on the side

--- src/components/FlowChartContainer.jsx
+++ src/components/FlowChartContainer.jsx
@@ -15,19 +15,29 @@ const FlowChartContainer = ({ rows }) => {
   return (
     <div className="flow-chart-container">
       {rows.length === 0 ? (
-        <div>Add some tasks to see them on the graph</div>
+        <div
+          style={{
+            fontSize: "40px",
+            textAlign: "center",
+            wordWrap: "break-word",
+          }}
+        >
+          Add some tasks to see them on the graph
+        </div>
       ) : (
         <Chart
           width={"100%"}
-          height={"100%"}
+          height={50 * rows.length < 900 ? 50 * rows.length : "100%"}
           columns={columns}
           rows={rows}
           chartType="Timeline"
           loader={<Loading />}
           options={{
-            timeline: { showRowLabels: false },
             tooltip: { isHtml: true },
+            timeline: { showRowLabels: false },
+            chartArea: { width: "100%", height: "100%" },
           }}
+          className="flow-chart"
         />
       )}
     </div>
--- src/pages/FlowView.jsx
+++ src/pages/FlowView.jsx
@@ -58,16 +58,25 @@ const FlowView = ({ history, match }) => {
       newRows[index] = [
         task.title,
         task.title,
-        `<p style="font-size: 20px; margin: 0px; padding: 10px;">${
+        `<p style="font-size: 25px; font-weight: bold; margin: 0px; padding: 10px; padding-bottom: 0px; text-align: center;">${
           task.title
+        }<br>${
+          task.tag.length !== 0
+            ? `
+            <p style="font-size: 15px; margin: 0px; text-align: center; padding-bottom: 0px;">
+              ${task.tag}
+            </p>`
+            : `
+            <p style="font-size: 15px; margin: 0px; text-align: center; padding-bottom: 0px;">
+            </p>`
         }</p><hr style="margin-left: 5px; margin-right: 5px; margin-top: 0px; margin-bottom: 0px;" />
-        <p style="font-size: 20px; margin: 0px; padding: 10px; padding-top: 0px; padding-bottom: 0px;">Users:${
+        <p style="font-size: 20px; margin: 0px; padding: 10px; padding-top: 0px; padding-bottom: 0px; text-align: center;">Users:${
           task.users.length !== 0
             ? task.users.map((user) => `<br>${user}`)
             : " No one is assigned to this task"
         }</p>
         <hr style="margin-left: 5px; margin-right: 5px; " />
-        <p style="font-size: 20px; margin: 0px; padding: 10px; padding-top: 0px;">${
+        <p style="font-size: 20px; margin: 0px; padding: 10px; padding-top: 0px; text-align: center;">${
           task.startDate
         } - ${task.endDate} <br>Duration: ${
           (new Date(task.endDate).getTime() -
