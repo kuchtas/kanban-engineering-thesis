20effacff495ba773f29da74db254728f53eb0fa
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-12-14 16:00:52 +0100

    Add PWA functionalities

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import AppWithAuth from './AppWithAuth';
-import Amplify,{ DataStore} from "aws-amplify";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import AppWithAuth from "./AppWithAuth";
+import Amplify, { DataStore } from "aws-amplify";
 import awsExports from "./aws-exports";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./service-worker";
 
 Amplify.configure(awsExports);
 DataStore.configure(awsExports);
@@ -13,10 +14,11 @@ ReactDOM.render(
   <React.StrictMode>
     <AppWithAuth />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
