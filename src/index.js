94d5e681a11fe4e83c5f1e45f6f4f605e071ed98
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-12-14 17:35:23 +0100

    Give up on PWA

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import AppWithAuth from "./AppWithAuth";
 import Amplify, { DataStore } from "aws-amplify";
 import awsExports from "./aws-exports";
 import reportWebVitals from "./reportWebVitals";
-import { ServiceWorker } from "aws-amplify";
-const serviceWorker = new ServiceWorker();
+
 Amplify.configure(awsExports);
 DataStore.configure(awsExports);
 
@@ -21,7 +20,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-const registeredServiceWorker = serviceWorker.register(
-  "/service-worker.js",
-  "/"
-);
