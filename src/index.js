202ddff350974d9373c84b8641d81445e8e2297b
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-12-14 17:11:14 +0100

    Fix PWA

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import AppWithAuth from "./AppWithAuth";
-import Amplify, { DataStore } from "aws-amplify";
+import Amplify, { DataStore, ServiceWorker } from "aws-amplify";
 import awsExports from "./aws-exports";
 import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./service-worker";
 
 Amplify.configure(awsExports);
 DataStore.configure(awsExports);
+const sw = new ServiceWorker();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,4 +22,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
+// serviceWorker.register();
+sw.register();
