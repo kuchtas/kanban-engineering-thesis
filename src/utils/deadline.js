2d52641904b753741121874297c8f51c380b1aae
Marcin Kuchta <marcinkuchta98@gmail.com>
2020-12-21 19:19:03 +0100

    Code reduction after a review

--- src/utils/deadline.js
+++ src/utils/deadline.js
@@ -1,41 +1,32 @@
 export const setClassByDeadlineCloseness = (startDate, endDate) => {
-  if(new Date() - new Date(startDate) < 0 ) return "deadline-term-0"
-  else{
-    if (new Date(endDate).setHours(23, 59, 59, 59) -
-        new Date(startDate).setHours(0, 0, 0, 0) > 604800000) { //seven days
-      let deadlineCloseness = Math.round(
-        ((new Date() - new Date(startDate).setHours(0, 0, 0, 0)) /
-          (new Date(endDate).setHours(23, 59, 59, 59) -
-            new Date(startDate).setHours(0, 0, 0, 0))) *  100);
-          if (deadlineCloseness >= 0 && deadlineCloseness <= 25)
-            return "deadline-term-1";
-          else if (deadlineCloseness > 25 && deadlineCloseness <= 50)
-            return "deadline-term-2";
-          else if (deadlineCloseness > 50 && deadlineCloseness <= 75)
-            return "deadline-term-3";
-          else if (deadlineCloseness > 75 && deadlineCloseness <= 100)
-            return "deadline-term-4";
-          else if (deadlineCloseness > 100) 
-            return "deadline-term-5";
-    }   
-    
-    else if (new Date(endDate).setHours(23, 59, 59, 59) - // less 7 days left - 1
-             new Date() < 0)
-              return "deadline-term-5";
-    else if (new Date(startDate).setHours(0, 0, 0, 0) -
-              new Date() > 0)
-               return "deadline-term-0";
-    else if (new Date(endDate).setHours(23, 59, 59, 59) -
-              new Date() > 518400000) // less 6 days left - 2
-              return "deadline-term-1";
-    else if (new Date(endDate).setHours(23, 59, 59, 59) -
-              new Date() > 345600000) // less 4 days left - 3
-              return "deadline-term-2";
-    else if (new Date(endDate).setHours(23, 59, 59, 59) -
-              new Date() > 172800000) // less 2 days left - 4
-              return "deadline-term-3";
-    else if (new Date(endDate).setHours(23, 59, 59, 59) -
-              new Date() > 0)
-              return "deadline-term-4";
+  const endDateLate = new Date(endDate).setHours(23, 59, 59, 59);
+  const startDateEarly = new Date(startDate).setHours(0, 0, 0, 0);
+  const currentDate = new Date();
+
+  if (currentDate - startDateEarly < 0) return "deadline-term-0";
+  if (endDateLate - currentDate < 0) return "deadline-term-5";
+  if (endDateLate - startDateEarly > 604800000) {
+    //seven days
+    let deadlineCloseness = Math.round(
+      ((currentDate - startDateEarly) / (endDateLate - startDateEarly)) * 100
+    );
+    if (deadlineCloseness >= 0 && deadlineCloseness <= 25)
+      return "deadline-term-1";
+    if (deadlineCloseness > 25 && deadlineCloseness <= 50)
+      return "deadline-term-2";
+    if (deadlineCloseness > 50 && deadlineCloseness <= 75)
+      return "deadline-term-3";
+    if (deadlineCloseness > 75 && deadlineCloseness <= 100)
+      return "deadline-term-4";
   }
-  };
+  if (endDateLate - currentDate > 518400000)
+    // 6 days
+    return "deadline-term-1";
+  if (endDateLate - currentDate > 345600000)
+    // 4 days
+    return "deadline-term-2";
+  if (endDateLate - currentDate > 172800000)
+    // 2 days
+    return "deadline-term-3";
+  if (endDateLate - currentDate > 0) return "deadline-term-4";
+};
